a5c817f9ce8b99c351da5ac64bc33c8f4dff4219
netease_zcm <bjzhouchengming@corp.netease.com>
2017-01-30 18:10:18 +0800

    1.3.0

--- helper/arrayHp.ts
+++ helper/arrayHp.ts
@@ -1,21 +1,30 @@
-import * as _ from 'lodash';
+import {
+    findIndex as _findIndex,
+    find as _find,
+    findLast as _findLast,
+    groupBy as _groupBy,
+    sum as _sum,
+    filter as _filter,
+    remove as _remove,
+    pullAt as _pullAt
+} from 'lodash';
 export namespace arrayHp {
     export const isArray = (obj: any) => {
         return obj instanceof Array;
     }
-    export const findIndex = _.findIndex;
+    export const findIndex = _findIndex;
 
-    export const find = _.find;
+    export const find = _find;
 
-    export const findLast = _.findLast;
+    export const findLast = _findLast;
 
-    export const groupBy = _.groupBy;
+    export const groupBy = _groupBy;
 
-    export const sum = _.sum;
+    export const sum = _sum;
 
-    export const filter = _.filter;
+    export const filter = _filter;
 
-    export const remove = _.remove;
+    export const remove = _remove;
 
-    export const pullAt = _.pullAt
+    export const pullAt = _pullAt
 }
--- helper/objHp.ts
+++ helper/objHp.ts
@@ -1,15 +1,20 @@
-import * as _ from 'lodash';
+import {
+    cloneDeep as _cloneDeep,
+    isEqual as _isEqual,
+    omit as _omit,
+    pick as _pick
+} from 'lodash';
 
 export namespace objHp {
-    export const cloneDeep = _.cloneDeep;
+    export const cloneDeep = _cloneDeep;
 
     export const assignNewObj = (...objs: tCommon.allowVoid<Object>[]) => {
         return Object.assign({}, ...objs);
     }
 
-    export const isEqual = _.isEqual;
+    export const isEqual = _isEqual;
 
-    export const omit = _.omit;
+    export const omit = _omit;
 
-    export const pick = _.pick;
+    export const pick = _pick;
 }
