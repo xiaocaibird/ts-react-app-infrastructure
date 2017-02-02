ff0f74b27d68cab9838ba9002ec60eb93550185e
xiaocaibird <cmxdll@sina.com>
2017-02-02 21:45:23 +0800

    1.3.3

--- class/User/AUser.ts
+++ class/User/AUser.ts
@@ -1,4 +1,7 @@
 export abstract class AUser<T> {
     abstract getUser(...p: any[]): T | undefined;
     abstract isLogin(...p: any[]): boolean;
+    abstract checkLogin(...p: any[]): void | boolean;
+    abstract clearUser(...p: any[]): void;
+    abstract logout(...p: any[]): void;
 }
